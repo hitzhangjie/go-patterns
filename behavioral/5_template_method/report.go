75edc2331b6552fd08e9e155f240bccdaefa64c4
hitzhangjie <hit.zhangjie@gmail.com>
2024-11-10 22:42:41 +0800

    template-method: document composite isn't inheritance

--- behavioral/5_template_method/report.go
+++ behavioral/5_template_method/report.go
@@ -2,6 +2,46 @@ package templatemethod
 
 import "fmt"
 
+// In go, there's no inheritance, so we use composition instead of inheritance.
+//
+// A very common mistake is define GenerateReport() as a method in Reporter interface{},
+// and implement it in baseReport, like this:
+//
+// ```
+// type Reporter interface {
+//     GenerateReport()
+//     ...
+// }
+//
+// type baseReport struct {}
+//
+// func (br *baseReport) GenerateReport() {
+//     br.initializeReport()
+//     br.collectData()
+//     br.analyze()
+//     br.finalize()          // <= this is a mistake!
+//     br.finalReport()       // <= this is a mistake!
+// }
+//
+// type FinancialReport struct {
+//     baseReport
+// }
+//
+// func (fr *FinancialReport) finalize() { ... }
+// func (fr *FinancialReport) finaleport() { ... }
+//
+// Then you may want to this:
+//
+// ```
+// var r = &FinancialReport{}
+// r.GenerateReport() // <= this will call baseReport's GenerateReport(),
+//                    // <= but this template method will call baseReport's finalize() and finaleport(), not FinancialReport's finalize(), and finaleport().
+//                    // <= why? because composition is not inheritance! baseReport isn't superclass of FinancialReport!
+//                    // <=      in golang, there's no abstract class, only interface and concreate class..
+//
+// An easy way to work around this, is define the Template Method as package level function.
+// ```
+
 type Reporter interface {
 	initializeReport()
 	collectData()
